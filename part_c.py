2d8e57a347ca6cc51f89c76c71e6af996a71a378
tohar <tohar@200.co>
2022-01-25 12:26:45 +0200

    start c

---
+++ part_c.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from part_a import *
+
+UNIPORT_PATH_2020 = "data/covid19 - 2020.gb"
+UNIPORT_PATH_2020 = "data/covid19 - 2022.gb"
+
+if __name__ == "__main__":
+    pass
